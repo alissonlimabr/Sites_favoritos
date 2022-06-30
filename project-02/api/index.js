d19363abb22765517769dd4e6b0d0a41213104b8
Lucas Lima <15lucas.lima15@gmail.com>
2022-06-29 19:40:13 -0500

    Aula 29/06/2022

--- project-02/api/index.js
+++ project-02/api/index.js
@@ -1,21 +1,41 @@
-const fs = require('http')
+const http = require('http')
 const data = require('./urls.json')
 const URL = require('url')
+const fs = require('fs')
+const path = require('path')
 
-fs.createServer((req, res) => { 
+http.createServer((req, res) => { 
 
     // const dados = JSON.stringify(data)
 
+
     const {name, url, del} = URL.parse(req.url, true).query
-    
+
+
     if (!name || !url)
-        return res.end('show')
+        return res.end(JSON.stringify(data))
 
-    if (del)
-        return res.end('delete')
+    if (del) {
+        data.urls = data.urls.filter(item =>item.url != url)
+        return writeFile(message => res.end(message));
+    }
+ 
 
-    return res.end('create')
+        data.urls.push(name, url)
+        return writeFile(message => res.end(message))
 
 
 
 }).listen(3000, () => console.log('API is running.'))
+
+function writeFile(cb)
+{
+    fs.writeFile(
+    path.join(__dirname, 'urls.json'),
+    JSON.stringify(data, null, 2),
+    err => {
+        if(err) throw err
+        cb('Operação realizada com sucesso!')
+        }
+    )
+}
